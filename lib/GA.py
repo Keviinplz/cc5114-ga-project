4f0604f5988d8c648b0e664adb7978b83f1daae7
didotau <estebanjramirezxcvi@gmail.com>
2020-11-24 18:31:48 -0300

    Hi gitman, i made a little (very little) change in GA.py

--- lib/GA.py
+++ lib/GA.py
@@ -12,60 +12,39 @@ class GA():
 
     p_s: int
         Size of the Population
-    i_l: int
+    b_s: int
         Length of an individual word
-    spanish: bool
-        Use the Spanish alphabet
+
     population: list
         List of all individual
     """
     population = None
 
-    def __init__(self, p_s: int, i_l: int, spanish=False):
+    def __init__(self, p_s: int, b_s: int):
         """
         Parameters
         -----------
         p_s: int
             The size of the Population, in this case, the GA Class will have
             a constant population size
-        i_l: int
-            The Length of an Individual Word, remeber that an individual is
-            constant respect to his length, but it can be mutated
-        spanish: bool
-            Determine if we use the Spanish Alphabet instead of English Alphabet
+        b_s: int
+            The size of the chessboard
+
         """
         self.p_s = p_s
-        self.i_l = i_l
-        self.spanish = spanish
-    
-    @staticmethod
-    def getAlphabet(spanish=False) -> str:
-        """
-        Return a string with the alphabet that we gonna use
+        self.b_s = b_s
         
-        Parameters
-        ----------
-        spanish: bool
-            If if true, the Alphabet gonna be spanish, by default the alphabet gonna be in english
-        """
-        spanish_alphabet = "abcdefghijklmnñopqrstuvwxyzáéíóúäëïöü"
-        english_alphabet = "abcdefghijklmnopqrstuvwxyz"
-        return spanish_alphabet if spanish else english_alphabet
+    
+   
         
 
     def createPopulation(self) -> None:
         """
-        Create a population of p_s individuals with length i_l
+        Create a population of p_s boards with b_s size
 
         """
-        alphabet = self.getAlphabet(spanish=self.spanish)
-        tempPopulation = []
-        for i in range(self.p_s):
-            tempIndividual = []
-            for j in range(self.i_l):
-                tempIndividual.append(random.choice(alphabet))
-            tempPopulation.append(np.array(tempIndividual))
-        self.population = np.array(tempPopulation)
+
+        self.population = np.random.randint(8, size = (self.p_s, self.b_s))
     
     def getPopulation(self) -> list:
         """
