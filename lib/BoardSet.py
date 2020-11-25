a144847f76fe0d00bb9fed103b411eec51d4c251
didotau <estebanjramirezxcvi@gmail.com>
2020-11-25 12:55:45 -0300

    I think this may work, take a look

---
+++ lib/BoardSet.py
@@ -0,0 +1,59 @@
+import random
+import collections
+import numpy as np
+
+class BoardSet():
+    """
+    Define the board set that contains the queen's position
+
+    Attributes
+    -----------
+
+    conf: configuration array
+    b_s: board size
+    """
+    def __init__(self, conf: list, b_s: int):
+        self.conf = conf
+        self.b_s = b_s
+    
+    def getFitness(self)->int:
+        """Returns the fitness function of a chessboard
+
+        Returns:
+            int: [description]
+        """
+        
+        fitness = 0
+        collection = collections.Counter(self.conf)
+        for key in collection:
+            fitness += collection[key]-1
+        
+        for i in range(self.b_s):
+            for j in range(i, self.b_s):
+                diff_index=j-i
+                diff_pos=abs(self.conf[j]-self.conf[i])
+                if diff_index==diff_pos: fitness+=1
+        return fitness             
+        
+    def getConfig(self)->list:
+        """ Returns array of queens position
+
+        Returns:
+            list: config array
+        """
+
+        return self.conf
+
+    def mutate(self, mutation_prob: float):
+        """Mutate the genetic of the individual by changing an aleatory position with a random number
+
+        Args:
+            mutation_prob (float): mutation change derivate from the GA atributes
+        """
+        random_float= random.random()
+        if(random_float<= mutation_prob):
+            random_index= random.randint(0, self.b_s-1)
+            random_position = random.randint(0, self.b_s-1)
+            self.conf[random_index] = random_position
+
+
--- lib/GA.py
+++ lib/GA.py
@@ -1,6 +1,7 @@
 import random
-
 import numpy as np
+
+from BoardSet import BoardSet
 
 
 class GA():
@@ -14,13 +15,15 @@ class GA():
         Size of the Population
     b_s: int
         Length of an individual word
-
     population: list
         List of all individual
+    mut_chance: float
+        mutation chance indicate how likely an individual is to be mutated
     """
     population = None
+    ready = False
 
-    def __init__(self, p_s: int, b_s: int):
+    def __init__(self, p_s: int, b_s: int, s_f: float, mut = 0.8):
         """
         Parameters
         -----------
@@ -30,9 +33,13 @@ class GA():
         b_s: int
             The size of the chessboard
 
+        s_f: float
+            Selection factor 
         """
         self.p_s = p_s
         self.b_s = b_s
+        self.s_f = s_f
+        self.mut = mut
         
     
    
@@ -43,8 +50,11 @@ class GA():
         Create a population of p_s boards with b_s size
 
         """
+        pop = np.random.randint(8, size = (self.p_s, self.b_s))
+        for i in range(self.p_s):
+            i_board = BoardSet(pop[i].tolist(), self.b_s)
+            self.population.append(i_board)
 
-        self.population = np.random.randint(8, size = (self.p_s, self.b_s))
     
     def getPopulation(self) -> list:
         """
@@ -52,5 +62,46 @@ class GA():
         """
         return self.population
 
+    def sortPopulation(self) -> list:
+        """Sort the population in ascending order of
+
+        Returns:
+            list: [description]
+        """
+        fitness_arr = []
+        for i in range(self.p_s):
+            fitness_arr.append(self.population[i].getFitness())
+        
+        sort_index = np.argsort(fitness_arr)
+        self.population = self.population[sort_index]
+
+    def selectParents(self)->list:
+        """Order the population and select a parents such as
+            the better fitness function are priorizated 
+
+        Returns:
+            list: [description]
+        """
+        
+        self.sortPopulation()
+        rand = np.random.uniform(0, 1, 2) # two parents
+        random_index = self.p_s * rand**(self.s_f)
+        random_index.astype(int)
+        parents = self.population[random_index]
+        return parents
+        
+    def reproduce(self, parents:list)->list:
+        cross_point = random.randint(1, self.b_s-1)
+        firstParent = parents[0]
+        secondParent = parents[1]
+        firstChild = BoardSet(firstParent.getConfig()[:cross_point].append(secondParent.getConfig()[cross_point:]), self.b_s)
+        secondChild = BoardSet(secondParent.getConfig()[:cross_point].append(firstParent.getConfig()[cross_point:]), self.b_s)
+        firstChild.mutate(self.mut)
+        secondChild.mutate(self.mut)
+        children = [firstChild, secondChild]
+        return children
+
+    def isReady(self):
+        return self.ready
     
 
