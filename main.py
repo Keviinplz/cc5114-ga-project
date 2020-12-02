0a5b80ec80799ea9e35d42bb7f4e330b9b01df4a
Keviinplz <kevin.pinochet@ing.uchile.cl>
2020-12-01 23:34:14 -0300

    Finished

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
+import sys
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 from lib.GA import GA
 
+def handleInputs():
+    if len(sys.argv) != 5:
+        raise ValueError("""
+        Bad parameters, usage:
+        python3 main.py <board_size> <population> <selection_factor> <mutation>
+        """)
+    if int(sys.argv[1]) % 2 != 0:
+        raise ValueError("""
+        Board Size has to be even.
+        """)
+
+    mutation = float(sys.argv[4])
+    if not (0 <= mutation and mutation <= 1):
+        raise ValueError("""
+        Mutation is a probability parameter, must to be between [0, 1]
+        """)
+
 if __name__ == '__main__':
-    board_size =  8
-    pop_size = 60
-    selection_factor = 5
-    ga = GA(pop_size, board_size, selection_factor)
+    handleInputs()
+    board_size = int(sys.argv[1])
+    pop_size = int(sys.argv[2])
+    selection_factor = int(sys.argv[3])
+    mutation = float(sys.argv[4])
+    ga = GA(pop_size, board_size, selection_factor, mutation)
     ga.createPopulation()
     best_fitness = []
     while not ga.isReady():
