c7cc1bdaa16a05c8e7991802e23809ebb37adf6c
didotau <estebanjramirezxcvi@gmail.com>
2020-12-01 22:00:33 -0300

    I solved a problem with a name jeje

--- main.py
+++ main.py
@@ -7,20 +7,20 @@ if __name__ == '__main__':
     board_size =  8
     pop_size = 60
     selection_factor = 5
-    GAlg = GA(pop_size, board_size, selection_factor)
-    GAlg.createPopulation()
+    ga = GA(pop_size, board_size, selection_factor)
+    ga.createPopulation()
     best_fitness = []
-    while not GAlg.isReady():
+    while not ga.isReady():
         next_generation = []
         for i in range(int(pop_size/2)):
             parents = ga.selectParents()
             children = ga.reproduce(parents)
             next_generation.append(children[0])
             next_generation.append(children[1])       
-        GAlg.setPopulation(next_generation)
-        GAlg.sortPopulation()
-        best_fitness.append(GAlg.getPopulation()[0].getFitness())
-c_winner = GAlg.getPopulation()[0].getConfig()
+        ga.setPopulation(next_generation)
+        ga.sortPopulation()
+        best_fitness.append(ga.getPopulation()[0].getFitness())
+c_winner = ga.getPopulation()[0].getConfig()
 # printing the board that solves the n-queen problem
 print(c_winner)
 # plotting the results
